0ec2adb0bcf4509e1bac2023ea3efaad30c05fe1
Cherry2 <Cherry2>
2016-05-25 10:17:58 -0400

    fix exception

--- src/Principal.java
+++ src/Principal.java
@@ -1,70 +1,3 @@
-/*
-import code.animal.*;
-import code.top.AnimalVertebrado;
-import code.lib.EstadoMetamorfosis;
-import code.lib.TipoReproduccion;
-import code.sangre.*;
-
-public class Principal {
-
-	public static void main(String[] args) {
-		
-		AnimalVertebrado frog = new Sapo("Permiable por agua, lycra","Piernas",4, "Carnivoro",
-				"Caminar, nadar, saltar","Sapo", TipoReproduccion.OVULIPARO,37.05,44.88,
-				EstadoMetamorfosis.ADULTO);	
-		System.out.println(frog.mostrarClasificacionCientifica());
-		System.out.println("\n");
-		frog.mostrarAnimalVertebrado();
-		System.out.println("\n");
-		frog.mostrarForma();
-		System.out.println("\n");
-		((SangreFria) frog).calcularTempActual();
-		System.out.println("------------------------------------------------------\n");
-		
-		AnimalVertebrado chameleon = new Camaleon("Escamas","Piernas",4,
-				"Carnivoro","Arrastrar","Camaleon",TipoReproduccion.OVIPARO,38.71,39.12,"Squamata");
-		System.out.println(chameleon.mostrarClasificacionCientifica());
-		System.out.println("\n");
-		chameleon.mostrarAnimalVertebrado();
-		System.out.println("\n");
-		chameleon.mostrarForma();
-		System.out.println("\n");
-		((SangreFria) chameleon).calcularTempActual();
-		System.out.println("------------------------------------------------------\n");
-		
-		AnimalVertebrado salmon = new Salmon("Escamas","Aletas",5,"Omnivoro",
-				"Nadar","Salmon",TipoReproduccion.OVULIPARO,44.45,44.45,2,3);
-		System.out.println(salmon.mostrarClasificacionCientifica());
-		System.out.println("\n");
-		salmon.mostrarAnimalVertebrado();
-		System.out.println("\n");
-		salmon.mostrarForma();
-		System.out.println("\n");
-		((SangreFria) salmon).calcularTempActual();		
-		System.out.println("------------------------------------------------------\n");
-		
-		AnimalVertebrado gorrion = new GorrionComun("Plumas","Alas",2,"Herbivoro",
-				"Volar","Gorrion Comun",TipoReproduccion.OVIPARO,"Evaporation",21.082);
-		System.out.println(gorrion.mostrarClasificacionCientifica());
-		System.out.println("\n");
-		gorrion.mostrarAnimalVertebrado();
-		System.out.println("\n");
-		gorrion.mostrarForma();
-		System.out.println("\n");
-		System.out.println("------------------------------------------------------\n");
-		
-		AnimalVertebrado perro = new Perro("Pelo","Patas",4,"Omnivoro","Caminar","Perro labrador",
-			TipoReproduccion.VIVIPARO,"Agitarse",true,10);
-		System.out.println(perro.mostrarClasificacionCientifica());
-		System.out.println("\n");
-		perro.mostrarAnimalVertebrado();
-		System.out.println("\n");
-		perro.mostrarForma();
-		System.out.println("\n");
-		System.out.println("------------------------------------------------------\n");
-	}
-}
-*/
 import code.lib.*;
 import code.zoo.Zoologico;
 import code.animal.*;
@@ -112,6 +45,7 @@ public class Principal {
 		@SuppressWarnings("resource")
 		java.util.Scanner in = new java.util.Scanner(System.in);
 		int opcion;
+		String opcionStr;
 		String buscar;
 		boolean continuarVisita = true;
 		
@@ -123,7 +57,10 @@ public class Principal {
 			System.out.println("\t 3) Buscar animal por nombre");
 			System.out.println("\t 4) Salir");
 			
-			opcion = in.nextInt();
+			opcionStr = in.next();
+			try{opcion = Integer.parseInt(opcionStr);}
+				catch (java.util.InputMismatchException m){ opcion =  0;}
+				catch (Exception e){ opcion =  0;}
 			
 			switch (opcion) {
 			case 1 :
